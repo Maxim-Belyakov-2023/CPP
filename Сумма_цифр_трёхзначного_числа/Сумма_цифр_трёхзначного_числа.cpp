cf309e0c4b4afe68672a0ed76986fb86547b59e1
TheBlueLegOLusha <TheBlueLegOLusha@mail.ru>
2023-08-03 18:08:34 +0300

    Решение задачи

--- Сумма_цифр_трёхзначного_числа/Сумма_цифр_трёхзначного_числа.cpp
+++ Сумма_цифр_трёхзначного_числа/Сумма_цифр_трёхзначного_числа.cpp
@@ -3,8 +3,18 @@
  * Необходимо вывсти на экран сумму его цифр.
  * Предполагается, что число трёхзначное.
  */
+#include<iostream>
+using namespace std;
+
 int main(int argc, char* argv[])
 {
-    
+    int number;
+    cin >> number;
+    int sum = 0;
+    sum += number % 10;
+    sum += (number / 10) % 10;
+    sum += number / 100;
+    cout << sum;
+
     return 0;
 }
