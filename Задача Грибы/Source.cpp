43571c84a12e364cd0df7a6d70441f389103bfe2
TheBlueLegOLusha <TheBlueLegOLusha@mail.ru>
2023-08-10 18:33:18 +0300

    Решение задач

---
+++ Задача Грибы/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	int number;
+	cin >> number;
+	if (number%100 >= 11 && number%100 <=14)
+		cout << number << " грибов" << endl;
+	else if (number % 10 == 1) {
+		cout << number << " гриб" << endl;
+	}
+	else if (number % 10 >= 2 && number % 10 < 5) {
+		cout << number << " гриба" << endl;
+	}
+	else {
+		cout << number << " грибов" << endl;
+	}
+
+}
