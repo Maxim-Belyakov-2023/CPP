57647f35eb13f9625a6eaccb82f551e034e863c0
TheBlueLegOLusha <TheBlueLegOLusha@mail.ru>
2023-08-18 12:05:48 +0300

    Исправление таблицы Пифагора

--- Таблица_Пифагора/Таблица_Пифагора.cpp
+++ Таблица_Пифагора/Таблица_Пифагора.cpp
@@ -2,10 +2,9 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    cout << " ";
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
-            cout << i * j << " ";
+            cout << left << setw(5)  << i * j << " ";
         }
         cout << endl;
     }
