b3f2b4afa7943c18a934b0af251bfe96bab2156f
Андрей Боев <a.boev.it@gmail.com>
2023-09-19 19:52:37 +0300

    Проврка ДЗ

--- Максимальная_цифра/Максимальная_цифра.cpp
+++ Максимальная_цифра/Максимальная_цифра.cpp
@@ -3,21 +3,26 @@ using namespace std;
 
 int max_digit(int num)
 {
-    if (num/10==0)
+    if (num / 10 == 0)
     {
         return num;
     }
-    else
-    {
-        return max(max_digit(num/10),num%10);
-    }
-    
+
+    return max(max_digit(num / 10), num % 10);
 }
+
+int max_digit2(int num, int max_num)
+{
+    if (num > 0)
+    {
+        return max_digit2(num / 10, (num % 10 > max_num ? num % 10 : max_num));
+    }
+    return max_num;
+}
+
 int main(int argc, char* argv[])
 {
-    int number,max,temp;
-    max = 0;
-    temp = 0;
+    int number;
     cin >> number;
-    cout << max_digit(number);
+    cout << max_digit2(number, 0);
 }
--- Сумма_цифр/Сумма_цифр.cpp
+++ Сумма_цифр/Сумма_цифр.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int sum_digit(int number)
 {
     if (number > 0)
     {
-        return sum_digit(number/10) + number%10;
+        return sum_digit(number / 10) + number % 10;
     }
     return 0;
 }
+
 int main(int argc, char* argv[])
 {
     int number;
     cin >> number;
-    cout<< sum_digit(number);
+    cout << sum_digit(number);
     return 0;
 }
