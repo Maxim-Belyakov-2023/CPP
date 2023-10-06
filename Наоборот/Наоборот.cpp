003cfa6a5b9782b4d847cc6263be41540cdc0635
Андрей Боев <a.boev.it@gmail.com>
2023-10-06 20:16:00 +0300

    Проверка ДЗ (требуется доработка)

--- Наоборот/Наоборот.cpp
+++ Наоборот/Наоборот.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
+
+void Reverse(int* array, int size)
+{
+    // TODO
+}
+
 int main(int argc, char* argv[])
 {
-    int number,chislo;
+    int number;
     cin >> number;
-    int* arr = new int[number]{};
-    for (int j = number-1; j>=0; j--)
-    {
-        cin >> chislo;
-        arr[j]=chislo;
-    }
-    for (int i = 0; i<number; i++)
-    {
-        cout<<arr[i]<< " ";
-    }
+    int* arr = new int[number];
+    for (int i = 0; i < number; i++)
+        cin >> arr[i];
     
+    Reverse(arr, number);
     
+    for (int i = 0; i < number; i++)
+        cout << arr[i] << " ";
     return 0;
 }
