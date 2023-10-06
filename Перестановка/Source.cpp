a7a0b9580f86f98f65b84caa2f1f0bf2d54a9069
Андрей Боев <a.boev.it@gmail.com>
2023-10-06 20:08:43 +0300

    FIX

--- Перестановка/Source.cpp
+++ Перестановка/Source.cpp
@@ -19,6 +19,4 @@ int main() {
 	for (int i = 0; i < count; i++) {
 		cout << arr[i];
 	}
-	
-	
 }
--- Шеренга/Source.cpp
+++ Шеренга/Source.cpp
@@ -1,27 +1,26 @@
 #include<iostream>
 using namespace std;
 
- int main(){
-	int number;
-	cin >> number;
-	int* arr = new int[number] {};
-	for (int i = 0; i < number; i++) {
-		cin >> arr[i];
-	}
-	for (int i = 0; i < number; i++) {
-		if (arr[i] < arr[number-1])
-		{
-			cout << i+1;
-			break;
-		}
-		if (arr[number - 2] == arr[number-1])
-		{
-			cout << number-1;
-		}
-	}
-	return 0;
-
-	
-
-
+int main()
+{
+    int number;
+    cin >> number;
+    int* arr = new int[number]{};
+    for (int i = 0; i < number; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < number; i++)
+    {
+        if (arr[i] < arr[number - 1])
+        {
+            cout << i + 1;
+            break;
+        }
+        if (arr[number - 2] == arr[number - 1])
+        {
+            cout << number - 1;
+        }
+    }
+    return 0;
 }
