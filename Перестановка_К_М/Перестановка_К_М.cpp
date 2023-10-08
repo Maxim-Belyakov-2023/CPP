abfbf7d9526ba28aa8c91b908ec07c3415c06a38
Андрей Боев <a.boev.it@gmail.com>
2023-10-08 18:08:13 +0300

    Проверка ДЗ (требуется доработка)

--- Перестановка_К_М/Перестановка_К_М.cpp
+++ Перестановка_К_М/Перестановка_К_М.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void K_M_Reverse(int* arr,int K, int M)
+
+void K_M_Reverse(int* arr, int K, int M)
 {
-    for (int i =0; i < M/2; i++)
+    for (int i = 0; i < M / 2; i++)
     {
-        int temp = arr[K+i];
-        arr[K+i]=arr[M-i];
-        arr[M-i]=temp;
+        int temp = arr[K + i];
+        arr[K + i] = arr[M - i];
+        arr[M - i] = temp;
     }
 }
+
+/*  TODO: Не работает при некоторых тестах
+    Test1
+    8
+    1 2 3 4 5 6 7 8
+    5 8
+    Expected: 1 2 3 4 8 7 6 5
+*/
 int main(int argc, char* argv[])
 {
-    int number,K,M;
+    int number, K, M;
     cin >> number;
     int* arr = new int[number];
-    for (int i = 0; i<number;i++)
+    for (int i = 0; i < number; i++)
     {
         cin >> arr[i];
     }
     cin >> K >> M;
-    K-=1;
-    M-=1;
-    K_M_Reverse(arr,K,M);
-    for (int i =0; i<number;i++)
+    K -= 1;
+    M -= 1;
+    K_M_Reverse(arr, K, M);
+    for (int i = 0; i < number; i++)
     {
         cout << arr[i] << " ";
     }
