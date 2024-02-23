00b1ec84d6e882f35adcdeb038dd840ec1d99771
Андрей Боев <a.boev.it@gmail.com>
2024-02-23 19:56:54 +0300

    Тестирование оператора присваивания

--- Перегрузка_операторов/Перегрузка_операторов.cpp
+++ Перегрузка_операторов/Перегрузка_операторов.cpp
@@ -11,14 +11,16 @@ int main(int argc, char* argv[])
     Matrix matrix2(2);
     matrix1.RandomGenerate();
     matrix2.RandomGenerate();
-    std::cout << matrix1 << '\n';
-    std::cout << matrix2 << '\n';
-    std::cout << matrix1 + matrix2 << '\n';
+    std::cout << matrix1 << "\n\n";
+    std::cout << matrix2 << "\n\n";
+    std::cout << matrix1 + matrix2 << "\n\n";
     matrix1++;
-    std::cout << matrix1 << '\n';
+    std::cout << matrix1 << "\n\n";
     if (matrix1 != matrix2)
-        std::cout << "Not Same\n";
-    std::cout << "Det = " <<static_cast<double>(matrix1);
+        std::cout << "Not Same\n\n";
+    std::cout << "Det = " << static_cast<double>(matrix1) << "\n";
+
+    Matrix matrix3 = matrix1;
+    std::cout << matrix3;
     return 0;
 }
-
