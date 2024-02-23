3e57717705c9883158971aa13bc760de524f57ca
TheBlueLegOlusha <TheBlueLegOlusha@mail.ru>
2024-02-23 18:42:18 +0300

    Исправление недочетов

--- Исключительные_ситуации/Исключительные_ситуации.cpp
+++ Исключительные_ситуации/Исключительные_ситуации.cpp
@@ -10,21 +10,17 @@ int main(int argc, char* argv[])
     setlocale(LC_ALL, "RU");
     try
     {
-        Matrix matrix(2,2);
-        int a = matrix[3][3];
+        Matrix matrix3(2,2);
+        //int a = matrix3[3][3];
+        Matrix matrix(2,3);
+        Matrix matrix1(2,2);
+        matrix + matrix1;
     }
-    catch(out_of_range_error ex)
+    catch(out_of_range_error& ex)
     {
         std::cout << ex.what() << std::endl;
     }
-    try
-    {
-        Matrix matrix(2,3);
-        Matrix matrix1(2,2);
-        Matrix matrix3(10,10);
-        matrix + matrix1;
-    }
-    catch(incorrect_dimension_error ex)
+    catch(incorrect_dimension_error& ex)
     {
         std::cout << ex.what() << std::endl;
     }
